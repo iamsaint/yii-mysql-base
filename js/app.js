345b7fdc04ba859cfb33ef977fda7aef07e811f0
Alexey Chernousov <chernousov@game-insight.com>
2013-09-13 15:57:34 +0400

    Add bootstrap & AngularJS

---
+++ js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var app = angular.module('Baseapp',[]);
+
+app.run(['$rootScope', '$http', function(scope, $http){
+
+}]);
--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html >
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" ng-app="Trafbox">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" ng-app="Baseapp">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="language" content="en"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+        <link rel="stylesheet" type="text/css" media="all" href="/themes/classic/css/bootstrap.min.css">
     </head>
     <body>
         <?=$content?>
+        <script src="/js/vendors/angular.min.js"></script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
